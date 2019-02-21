dbd0f40825354731a657ed24e81836dc5f20cead
Ci cadas <mark@macbook-pro-mark.local>
2019-02-21 03:52:38 +0300

    Initial Commit

---
+++ LentaProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LentaProject
+//
+//  Created by Mark on 21/02/2019.
+//  Copyright © 2019 Mark. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
